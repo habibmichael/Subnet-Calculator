97e88b1d3ce527bd1b9a3a59f7c3c75964c5b71a
root <root@localhost.localdomain>
2016-01-02 04:55:13 -0800

    Convert mask to binary,Calculate the number of hosts per subnet, Calculate the wildcard mask

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -31,6 +31,31 @@ def subnet_calc():
 			else:#redirect to top of while loop
 				print "\nThe Subnet Mask is INVALID! Please retry!\n"
 				continue	
+		#Convert Mask to a binary string
+		mask_octet_decimal=subnet_mask.split('.')
+		mask_octet_binary=[]
+		for octet_index in range(0,len(mask_octet_decimal)):
+			#get rid of python formatting for binary numbers
+			binary_octet=bin(int(mask_octet_decimal[octet_index])).split('b')[1]
+			#Check to see if there are 8 bits, add to binary list
+			if len(binary_octet)==8:
+				mask_octet_binary.append(binary_octet)
+			elif len(binary_octet)<8:
+				#Pad to get 8 bits
+				binary_octet_padded=binary_octet.zfill(8)
+				mask_octet_binary.append(binary_octet_padded)
+		#Join list
+		decimal_mask="".join(binary_octet)
+		#Counting host bits and calculating number of hosts/subnet
+		no_of_zeros=decimal_mask.count("0")
+		no_of_ones=32-no_of_zeros
+		no_of_hosts=abs(2**no_of_zeros-2)
+		#Obtain wildcard mask
+		wildcard_octet=[]
+		for w_octect in mask_octet_decimal:
+			wild_octet=255-int(w_octect)
+			wildcard_octet.append(str(wild_octet))
+		wildcard_mask=".".join(wildcard_octet)
 
 
 	except KeyboardInterrupt:
